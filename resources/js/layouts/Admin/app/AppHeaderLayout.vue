0549e6fcc2926a59c08efd09d855252c79a4a702
tamim-ikbal <bmtamim28@gmail.com>
2026-03-19 12:47:07 +0600

    AdminLayout added

---
+++ resources/js/layouts/Admin/app/AppHeaderLayout.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import AppContent from '@/components/Admin/Layout/AppContent.vue';
+import AppHeader from '@/components/Admin/Layout/AppHeader.vue';
+import AppShell from '@/components/Admin/Layout/AppShell.vue';
+import type { BreadcrumbItem } from '@/types';
+
+type Props = {
+    breadcrumbs?: BreadcrumbItem[];
+};
+
+withDefaults(defineProps<Props>(), {
+    breadcrumbs: () => [],
+});
+</script>
+
+<template>
+    <AppShell variant="header">
+        <AppHeader :breadcrumbs="breadcrumbs" />
+        <AppContent variant="header">
+            <slot />
+        </AppContent>
+    </AppShell>
+</template>
---
+++ resources/js/layouts/Admin/app/AppSidebarLayout.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import AppContent from '@/components/Admin/Layout/AppContent.vue';
+import AppShell from '@/components/Admin/Layout/AppShell.vue';
+import AppSidebar from '@/components/Admin/Layout/AppSidebar.vue';
+import AppSidebarHeader from '@/components/Admin/Layout/AppSidebarHeader.vue';
+import type { BreadcrumbItem } from '@/types';
+
+type Props = {
+    breadcrumbs?: BreadcrumbItem[];
+};
+
+withDefaults(defineProps<Props>(), {
+    breadcrumbs: () => [],
+});
+</script>
+
+<template>
+    <AppShell variant="sidebar">
+        <AppSidebar />
+        <AppContent variant="sidebar" class="overflow-x-hidden">
+            <AppSidebarHeader :breadcrumbs="breadcrumbs" />
+            <slot />
+        </AppContent>
+    </AppShell>
+</template>
